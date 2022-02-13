4e4db71e903d2b7bd0e67871fbf8ee95bb05f23d
amin <aminqanbarii1992@gmail.com>
2022-02-13 04:22:49 -0800

    style productDetails

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,23 +1,33 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import styles from "./ProductDetails.module.css";
+import { Link } from "react-router-dom";
 
 const ProductDetails = () => {
   const { id } = useParams();
-  const [product , setProduct] = useState([])
+  const [product, setProduct] = useState([]);
+  const {image, title, description, price, category} = product;
+
   useEffect(() => {
-    axios.get(`https://fakestoreapi.com/products/${id}`)
-    .then(response => setProduct(response.data))
-  }, [id])
+    axios
+      .get(`https://fakestoreapi.com/products/${id}`)
+      .then((response) => setProduct(response.data));
+  }, [id]);
 
   return (
-    <div>
-      <img src={product.image} alt="product" />
-      <p>{product.price}</p>
-      <p><span>Category:</span>{product.category}</p>
-      <p>{product.description}</p>
+    <div className={styles.container}>
+    <img className={styles.image} src={image} alt="product" />
+    <div className={styles.textContainer}>
+        <h3>{title}</h3>
+        <p className={styles.description}>{description}</p>
+        <p className={styles.category}> {category} <span>: دسته بندی</span> </p>
+        <div className={styles.buttonContainer}>
+            <span className={styles.price}>{price} تومان</span>
+            <Link to="/products">بازگشت به فروشگاه</Link>
+        </div>
     </div>
+</div>
   );
 };
 
